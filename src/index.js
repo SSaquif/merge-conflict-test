4ee6b50b1f8906ed9630907758aa3934c6070a14
Sadnan Saquif <SSaquif@users.noreply.github.com>
2020-08-07 10:32:16 -0400

    Updated index.js directly from github

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div></div>
   </React.StrictMode>,
   document.getElementById('root')
 );
